10ffa5de5617d288a11f0214d54b75c150fad9ad
sgottschalk <seangottschalk@gmail.com>
2013-12-16 22:06:06 -0800

    links for artists

--- mfp/diviner/models.py
+++ mfp/diviner/models.py
@@ -48,8 +48,6 @@ class Artist(models.Model):
     """
     mbid = models.CharField(max_length=36, unique=True, null=True)
     songkickid = models.CharField(max_length=20, primary_key=True)
-    # mbid = models.CharField(max_length=36, primary_key=True)
-    # songkickid = models.CharField(max_length=20, unique=True, null=True)
     name = models.CharField(max_length=100)
     festival = models.ForeignKey(Festival)
     statuses = None
